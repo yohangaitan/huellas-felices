b69e7bcccd29d67973062de90c924e5901dd6ae0
Yohan Gaitan <gaitanyohann@gmail.com>
2025-11-29 16:26:58 -0500

    feat: Initial project setup (Django project and pets app)

---
+++ mascotas/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# Define el nombre de la aplicación para hacer referencia a las URLs fácilmente
+app_name = 'mascotas'
+
+urlpatterns = [
+    # path('', views.home_view, name='home'), # La vista la crearemos después
+]
